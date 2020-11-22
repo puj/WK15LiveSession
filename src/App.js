09997287a9ac202a1fa3231033cb003cba45ab52
Van <vango85@gmail.com>
2020-11-22 23:31:45 +0100

    [xx] Reverted to start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
 import React from "react";
-import { Provider } from "react-redux";
-import { todos } from "./reducers/todos";
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { List } from "components/List";
-
-const reducer = combineReducers({
-  todos: todos.reducer,
-});
-
-const store = configureStore({ reducer });
 export const App = () => {
-  return (
-    <Provider store={store}>
-      <List></List>
-    </Provider>
-  );
+  return <></>;
 };
